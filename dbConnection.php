1a58b45f3157a25b5bbf4568391cbdc55d25286f
kaysmart001 <luwoyekayode99@gmail.com>
2023-02-01 18:11:50 +0100

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('sql311.epizy.com','epiz_33459705','hvZgW4p2guu7','epiz_33459705_myexam_db')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
